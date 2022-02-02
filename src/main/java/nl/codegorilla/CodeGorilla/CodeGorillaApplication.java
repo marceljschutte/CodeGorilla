c23a6742291e0e4d33cb4b91b3de766749f246d1
Marcel Schutte <marceljschutte@gmail.com>
2022-02-02 17:12:59 +0100

    Initiele project setup

---
+++ src/main/java/nl/codegorilla/CodeGorilla/CodeGorillaApplication.java
@@ -0,0 +1,13 @@
+package nl.codegorilla.CodeGorilla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeGorillaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeGorillaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/codegorilla/CodeGorilla/CodeGorillaApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.codegorilla.CodeGorilla;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeGorillaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
