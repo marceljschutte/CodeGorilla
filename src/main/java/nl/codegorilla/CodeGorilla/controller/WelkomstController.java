7d7b4003e759b2b0edebdd0521f0487dd2ae6cd3
Marcel Schutte <marceljschutte@gmail.com>
2022-02-06 20:57:07 +0100

    aanpassingen voor opdracht

--- src/main/java/nl/codegorilla/CodeGorilla/controller/WelkomstController.java
+++ src/main/java/nl/codegorilla/CodeGorilla/controller/WelkomstController.java
@@ -1,13 +1,22 @@
 package nl.codegorilla.CodeGorilla.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HelloWorldController {
+public class WelkomstController {
 
     @RequestMapping("/welkom")
     public String helloworld(){
         return "Hallo, welkom bij jouw eerste spring boot applicatie";
+    }
+
+    @RequestMapping("/calculate")
+    public String calculate(@RequestParam int numOne, @RequestParam int numTwo, @RequestParam String operator){
+
+
+
+        return "";
     }
 }
