c2efa5610dedccc597d1a815cbcc669e867c6bbb
Marcel Schutte <marceljschutte@gmail.com>
2022-02-16 15:28:03 +0100

    Opdracht1 voor dag 5

---
+++ src/main/java/nl/codegorilla/CodeGorilla/exercises/dag5/exercise1/DutchZoosTest.java
@@ -0,0 +1,18 @@
+package nl.codegorilla.CodeGorilla.exercises.dag5.exercise1;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DutchZoosTest {
+
+    @Test
+    void testGetCity() {
+        /*Assertions.assertEquals("Amsterdam", DutchZoos.ARTIS.getCity());
+        Assertions.assertEquals("Arnhem", DutchZoos.BURGERS.getCity());
+        Assertions.assertEquals("Nuenen", DutchZoos.DIERENRIJK.getCity());
+        Assertions.assertEquals("Emmen", DutchZoos.WILDLANDS.getCity());*/
+    }
+
+}
